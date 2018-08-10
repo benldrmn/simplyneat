48df32615502bce86f875f3047b821c9b35076cb
Liron Bronfman <liron.bronfman@gmail.com>
2018-08-10 21:36:06 +0300

    Implemented adjusted fitness

--- simplyneat/population/population.py
+++ simplyneat/population/population.py
@@ -3,13 +3,16 @@ import random
 from simplyneat.genome.genome import Genome
 import logging
 
+from simplyneat.organism.organism import Organism
 from simplyneat.species.species import Species
 
 
 class Population:
-    def __init__(self):
+    def __init__(self, fitness_function, distance_threshold):
         self._list_of_species = []                          # a list of species
         self._organisms = []                                # a list of organisms
+        self._fitness_function = fitness_function           # TODO: maybe receive fitness function in config?
+        self._distance_threshold = distance_threshold       # TODO: receive distance_threshold in config
 
     def __add_organism(self, organism):
         assert organism not in self._organisms
@@ -33,6 +36,19 @@ class Population:
         # this is a new species!
         self._list_of_species.append(Species(organism))
 
+    def __calculate_adjusted_fitness(self, organism):
+        assert isinstance(organism, Organism)
+        assert organism in self._organisms
+        fitness = self._fitness_function(organism)
+        # at least 1 since the sharing of an organism with itself is 1
+        sum_of_sharing = sum([Population.__sharing_function(Genome.compatibility_distance(organism.genome(), other_organism.genome()))
+                         for other_organism in self._organisms])
+        return fitness/sum_of_sharing
 
-
+    @staticmethod
+    def __sharing_function(distance, distance_threshold):
+        if distance >= distance_threshold:          # TODO: make distance threshold a static variable
+            return 0
+        else:
+            return 1
 
