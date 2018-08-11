9e17003dd31ca216c35d44fcbf225d423d03df3f
benldrmn@gmail.com <Keglonah1991>
2018-08-11 19:21:15 +0300

    Started the process of defining the config defaults and passing the config object to the relevant constructors.

--- simplyneat/organism/organism.py
+++ simplyneat/organism/organism.py
@@ -2,10 +2,8 @@ from simplyneat.genome.genome import Genome
 
 
 class Organism:
-    def __init__(self, number_of_input_nodes, number_of_output_nodes, organism_index, fitness_function):
-        self._genome = Genome(number_of_input_nodes, number_of_output_nodes)
-        self._organism_index = organism_index
-        # TODO: for now, fitness_function is passed to the constructor
+    def __init__(self, config):
+        self._genome = Genome(config)
 
     @property
     def genome(self):
